2d3bd262116c0906d07a9607e4d490e494e376e6
Alireza Lorestani <alireza.lorestani@part.ir>
2020-10-04 17:48:28 +0330

    1 ui edit

---
+++ app/src/main/java/com/marathon/ktm/view/HomePageActivity.kt
@@ -0,0 +1,13 @@
+package com.marathon.ktm.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marathon.ktm.R
+
+class HomePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
